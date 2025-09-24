74053bc75301fecd612f1d8d91693b921a5cb63c
brian-kyalo <brianmuthama15@gmail.com>
2025-09-24 13:22:55 +0300

    setup: enviroment and project setup
    Created an Xcode project for developing my Application.

---
+++ Sprout/SproutApp.swift
@@ -0,0 +1,17 @@
+//
+//  SproutApp.swift
+//  Sprout
+//
+//  Created by Student1 on 24/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct SproutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
